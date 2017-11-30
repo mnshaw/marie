e49a4fa71efb96450ad3d54801494f59cfc6291f
mnshaw <mnshaw@andrew.cmu.edu>
2017-11-29 21:23:31 -0500

    Projects page for desktop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
+var currentProject = ".detours";
+var currentButton = "#detours-btn";
+
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		menu: '#menu',
 	});
+});
+
+$( "#detours-btn" ).click(function() {
+	$(currentProject).toggle();
+	$(currentButton).removeClass("btn-primary")
+  	currentProject = ".detours";
+  	currentButton = "#detours-btn"
+  	$(currentButton).addClass("btn-primary");
+  	$(currentProject).toggle();
+});
+
+$( "#gub-btn" ).click(function() {
+	$(currentProject).toggle();
+	$(currentButton).removeClass("btn-primary")
+  	currentProject = ".gubernator";
+  	currentButton = "#gub-btn"
+  	$(currentButton).addClass("btn-primary");
+  	$(currentProject).toggle();
+});
+
+$( "#docker-btn" ).click(function() {
+	$(currentProject).toggle();
+	$(currentButton).removeClass("btn-primary")
+  	currentProject = ".docker";
+  	currentButton = "#docker-btn"
+  	$(currentButton).addClass("btn-primary");
+  	$(currentProject).toggle();
+});
+
+$( "#research-btn" ).click(function() {
+	$(currentProject).toggle();
+	$(currentButton).removeClass("btn-primary")
+  	currentProject = ".research";
+  	currentButton = "#research-btn"
+  	$(currentButton).addClass("btn-primary");
+  	$(currentProject).toggle();
 });
