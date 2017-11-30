dc2c255ec1a7b15aa9bfcb2463537964a658476a
mnshaw <mnshaw@andrew.cmu.edu>
2017-11-29 23:08:58 -0500

    Takeout and fortune cookie

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var currentProject = ".detours";
 var currentButton = "#detours-btn";
 
+// Fortunes chosen from http://www.fortunecookiemessage.com/archive.php
+var fortunes = ["Hard work pays off in the future, laziness pays off now.", "A chance meeting opens new doors to success and friendship.", "You cannot love life until you live the life you love.", "You already know the answer to the questions lingering inside your head.", "You must try, or hate yourself for not trying.", "You can make your own happiness.", "If winter comes, can spring be far behind?", "Your ability for accomplishment will follow with success."];
+
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		menu: '#menu',
@@ -41,4 +44,9 @@ $( "#research-btn" ).click(function() {
   	currentButton = "#research-btn"
   	$(currentButton).addClass("btn-primary");
   	$(currentProject).toggle();
+});
+
+$( "#fortune-btn" ).click(function() {
+	var fortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+	$("#fortune-text").text(fortune);
 });
